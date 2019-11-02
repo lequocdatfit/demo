cd957ebb38a502a4ccecfe92b5789704d56f342c
lequocdatfit <48687245+lequocdatfit@users.noreply.github.com>
2019-11-02 19:07:43 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-import math
-def kiemtraNguyenTo(x):
-  if (x<=1):
-  	return False
-  for i in range(2,int(math.sqrt(x))+1):
-    print(i)
-    if (x%i==0):
-    	return False
-  return True
+a=128
+b=32
+dem=0
 
-print(kiemtraNguyenTo(26))
+if(a==0):
+	for i in range(1,b+1):
+		if(b%i==0):
+			dem=dem+1
+elif(b==0):
+	for i in range(1,a+1):
+		if(a%i==0):
+			dem=dem+1
+elif(a<b):
+    for i in range(1,a+1):
+        if(a%i==0 and b%i==0):
+            dem=dem+1
+else:
+    for i in range(1,b+1):
+        if(a%i==0 and b%i==0):
+            dem=dem+1
+print(dem)
